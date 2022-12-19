5b744aff1b7a090e8533674a309883030207fa46
Vipul <97651740+vipul124@users.noreply.github.com>
2022-12-19 14:52:55 +0530

    Update userRouter.go

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -9,4 +9,8 @@ import (
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users/forms", controllers.AllFormTemplates())
+	incomingRoutes.GET("/users/forms/:form_type", controllers.SelectForm())
+	// incomingRoutes.POST("/users/forms/:form_type", controllers.SubmitForm())
+	incomingRoutes.GET("/users/status", controllers.GetAllForms())
+	incomingRoutes.GET("/users/status/:form_id", controllers.GetForm())
 }
